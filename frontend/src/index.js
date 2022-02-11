bcd419b62e54a5affdbc00dba2c0b0df312f4791
lycho33 <cho.lyds@gmail.com>
2022-02-11 17:49:17 -0500

    add redux packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
