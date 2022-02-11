eb3bb5afaa8838d995d6a58e8545e36ad07f3e2f
lycho33 <cho.lyds@gmail.com>
2022-02-11 16:38:45 -0500

    created login routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: "users#new"
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
