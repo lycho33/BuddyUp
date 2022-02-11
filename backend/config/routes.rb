5ef716c38acc260ed556907c2de4c5211f2ab6e3
lycho33 <cho.lyds@gmail.com>
2022-02-11 17:07:19 -0500

    signup/login/signin routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/signup', to: "users#new"
-  resources :users
+  # resources :users
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  # get 'password', to: 'passwords#edit', as: 'edit_password'
+  # patch 'password', to: 'passwords#update'
+  # get 'password/reset', to: 'password_resets#new'
+  # post 'password/reset', to: 'password_resets#create'
+  # get 'password/reset/edit', to: 'password_resets#edit'
+  # patch 'password/reset/edit', to: 'password_resets#update'
 end
