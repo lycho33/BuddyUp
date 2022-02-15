01ed555628e39f4ea8390b24fcc82250c9f7afd9
lycho33 <cho.lyds@gmail.com>
2022-02-15 14:53:01 -0500

    add redux dir

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { reducer } from './redux/reducer'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : (null || compose);
 
-const store = createStore(
+const store = createStore(reducer, 
   composeEnhancers(applyMiddleware(thunk))
 );
 
