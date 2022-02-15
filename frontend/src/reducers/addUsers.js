526a63afdbf5c0500baaf4ae80ac7d1565ecde1e
lycho33 <cho.lyds@gmail.com>
2022-02-15 14:47:17 -0500

    addUsers fetch request + error handling

--- frontend/src/reducers/addUsers.js
+++ frontend/src/reducers/addUsers.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 export const addUser = (data, handleSuccess) => (dispatch) => {
     axios
         .post(
-            'http://localhost:3001/users'
+            'http://localhost:3001/users',
+            { user: data },
+            { withCredentials: true }
+        )
+        .then((response) => {
+            if(response.data.status === 500){
+                throw new Error(response.data.error)
+            }
+            dispatch({type: 'LOGIN_USER', user: response.data.user});
+            dispatch({type: "CLEAR_ERROR"});
+            handleSuccess();
+        })
+        .catch((response) => 
+            dispatch({type: 'ADD_ERROR', error: response.message})
         )
 }
