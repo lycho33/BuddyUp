c7e295a8637a0e830a99410775d0c39bb45e8c57
lycho33 <cho.lyds@gmail.com>
2022-02-15 16:21:12 -0500

    changed sessions_controllers

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,16 +1,25 @@
 class ApplicationController < ActionController::Base
-    include ActionController::Cookies
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    
-    before_action :authorize
+  skip_before_action :verify_authenticity_token
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
-    def authorize
-      @current_user = User.find_by(id: session[:user_id])
-  
-      render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-    end
-  
-    def render_unprocessable_entity_response(exception)
-      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
-    end
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorized_user?
+     @user == current_user
+   end
+
+  def logout!
+     session.clear
+   end
+   
 end
--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,21 +1,47 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only: :create
-    # def new
-    # end
-
+    
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticaste(params[:session][:password])
-            log_in user
-            redirect_back_or user
-        else 
-            flash.now[:danger] = 'Invalid email/password combination'
-            render 'new'
+        @user = User.find_by(email: session_params[:email])
+      
+        if @user && @user.authenticate(session_params[:password])
+          login!
+          render json: {
+            logged_in: true,
+            user: @user
+          }
+        else
+          render json: { 
+            status: 401,
+            errors: ['no such user', 'verify credentials and try again or signup']
+          }
+        end
+    end
+
+    def is_logged_in?
+        if logged_in? && current_user
+          render json: {
+            logged_in: true,
+            user: current_user
+          }
+        else
+          render json: {
+            logged_in: false,
+            message: 'no such user'
+          }
         end
     end
 
     def destroy
-        log_out
-        redirect_to root_url
+        logout!
+        render json: {
+          status: 200,
+          logged_out: true
+        }
+    end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
