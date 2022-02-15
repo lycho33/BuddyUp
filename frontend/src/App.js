a3ea1eb056bfa0f301a0536a6b9727609e1f9fd2
lycho33 <cho.lyds@gmail.com>
2022-02-15 16:35:55 -0500

    added routes + login/logout methods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignUp from './Signup'
 
 const App = () => {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState('')
+
+  const handleLogin = (data) => {
+    setIsLoggedIn(true)
+    setUser(data.user)
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+    setUser('')
+  }
+
   return (
     <div className="App">
       <SignUp/>
+
+      <Routes>
+        <Route exact path='/' element={} />
+        <Route exact path='/login' element={} />
+        <Route exact path='/signup' element={} />
+      </Routes>
     </div>
   );
 }
