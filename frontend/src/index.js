1f5bc757b97afbd5955d3df60c8bdefe3de3c8b9
lycho33 <cho.lyds@gmail.com>
2022-02-15 14:31:14 -0500

    delete root reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,6 @@ import { Provider } from 'react-redux';
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : (null || compose);
 
 const store = createStore(
-  // rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
