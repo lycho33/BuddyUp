6a94dfd94164c715ebe99559c4892bf830cefb56
lycho33 <cho.lyds@gmail.com>
2022-02-15 16:44:18 -0500

    method to change login-status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignUp from './Signup'
@@ -8,6 +8,25 @@ const App = () => {
   
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [user, setUser] = useState('')
+
+
+  useEffect(() => {
+    loginStatus()
+  })
+
+  const loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+    {withCredentials: true})
+
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
 
   const handleLogin = (data) => {
     setIsLoggedIn(true)
@@ -18,6 +37,7 @@ const App = () => {
     setIsLoggedIn(false)
     setUser('')
   }
+
 
   return (
     <div className="App">
--- frontend/src/redux/actionCreators.js
+++ frontend/src/redux/actionCreators.js
@@ -1,21 +1,15 @@
 import axios from 'axios';
 
-export const addUser = (data, handleSuccess) => (dispatch) => {
-    axios
-        .post(
-            'http://localhost:3001/users',
-            { user: data },
-            { withCredentials: true }
-        )
-        .then((response) => {
-            if(response.data.status === 500){
-                throw new Error(response.data.error)
-            }
-            dispatch({type: 'LOGIN_USER', user: response.data.user});
-            dispatch({type: "CLEAR_ERROR"});
-            handleSuccess();
-        })
-        .catch((response) => 
-            dispatch({type: 'ADD_ERROR', error: response.message})
-        )
+export const loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in',
+    {withCredentials: true})
+
+    .then(res => {
+        if(res.data.logged_in){
+            this.handleLogin(res)
+        } else {
+            this.handleLogout()
+        }
+    })
+    .catch(error => console.log('api errors:', error))
 }
