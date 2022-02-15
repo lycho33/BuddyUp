15e2731f611858fc702cef26f1c98a61c9dd2c57
lycho33 <cho.lyds@gmail.com>
2022-02-15 14:14:43 -0500

    updated User models validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
 
     validates :username, presence: true
-    validates :username, uniqueness: true
+    validates :username, uniqueness: { case_sensitive: false }
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
