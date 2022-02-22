e192c18b6bc3b3b78498e53c6fd41b22b6fd7fc6
lycho33 <cho.lyds@gmail.com>
2022-02-21 19:32:44 -0500

    created constants file to repeated use

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,8 @@
+//
+
+export const API_ROOT = 'http://localhost:3001';
+export const API_WS_ROOT = 'ws://localhost:3001/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
