7616feaac174c8a043e6b1c7fd8d0ed333e306c9
lycho33 <cho.lyds@gmail.com>
2022-03-01 15:11:36 -0500

    added user_id params to messages

--- backend/app/channels/conversations_channel.rb
+++ backend/app/channels/conversations_channel.rb
@@ -2,8 +2,10 @@ class ConversationsChannel < ApplicationCable::Channel
   # This channel broadcasts newly created conversations
 
   def subscribed
+    user = params['username']
     # expects to receive a string as an argument
     stream_from "conversations_channel"
+    ActionCable.server.broadcast 'public_chat', "#{user} joined!"
   end
 
   def unsubscribed
--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -19,6 +19,6 @@ class MessagesController < ApplicationController
     private
   
     def message_params
-      params.require(:message).permit(:text, :conversation_id)
+      params.require(:message).permit(:text, :conversation_id, :user_id)
     end
 end
