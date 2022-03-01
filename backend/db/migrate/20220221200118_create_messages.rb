cb4509df5b911668ccd6f3db425aad957074fa44
lycho33 <cho.lyds@gmail.com>
2022-03-01 13:32:22 -0500

    seed a message

--- backend/db/migrate/20220221200118_create_messages.rb
+++ backend/db/migrate/20220221200118_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
     create_table :messages do |t|
       t.string :text
       t.references :conversation, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 2022_02_21_200118) do
   create_table "messages", force: :cascade do |t|
     t.string "text"
     t.integer "conversation_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_02_21_200118) do
   end
 
   add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
