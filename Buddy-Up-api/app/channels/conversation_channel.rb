7234c0ba4fd3d2a8c30944d085da879a007c6824
lycho33 <cho.lyds@gmail.com>
2022-03-14 22:36:31 -0400

    created conversations channel

---
+++ Buddy-Up-api/app/channels/conversation_channel.rb
@@ -0,0 +1,15 @@
+class ConversationChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    @conversation = Conversation.find_by(id: params[:conversation])
+    stream_for @conversation
+  end
+
+  def received(data)
+    ConversationChannel.broadcast_to(@conversation, {conversation: @conversation, users: @conversation.users, messages: @conversation.messages})
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- Buddy-Up-api/app/models/message.rb
+++ Buddy-Up-api/app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :conversation
-  belongs_to :sender, class_name: :User, foreign_key: 'sender_id'
+  belongs_to :sender, class_name: :User, foreign_key: 'user_id'
+
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
