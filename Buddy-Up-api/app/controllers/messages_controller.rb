db36efa2749e79709127567043b719e404fa6ba5
lycho33 <cho.lyds@gmail.com>
2022-03-15 21:47:36 -0400

    added message index method + updated conversations serializer

--- Buddy-Up-api/app/controllers/messages_controller.rb
+++ Buddy-Up-api/app/controllers/messages_controller.rb
@@ -1,2 +1,8 @@
 class MessagesController < ApplicationController
+    skip_before_action :require_login, only: [:index]
+
+    def index
+        messages = Message.all
+        render json: messages
+    end
 end
--- Buddy-Up-api/app/serializers/conversation_serializer.rb
+++ Buddy-Up-api/app/serializers/conversation_serializer.rb
@@ -1,3 +1,5 @@
 class ConversationSerializer < ActiveModel::Serializer
   attributes :id, :title
+  has_many :users
+  has_many :messages
 end
