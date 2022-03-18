976d21086511f49af3df76bc36087688c5de843a
lycho33 <cho.lyds@gmail.com>
2022-03-18 14:07:45 -0400

    broadcast messages to receive data on the frontend

--- Buddy-Up-api/app/channels/conversation_channel.rb
+++ Buddy-Up-api/app/channels/conversation_channel.rb
@@ -1,14 +1,11 @@
 class ConversationChannel < ApplicationCable::Channel
   def subscribed
     @conversation = Conversation.find_by(id: params[:conversation])
-    # @conversation = Conversation.find(params[:conversation])
     stream_from @conversation
+    ActionCable.server.broadcast(@conversation, { messages: @conversation.messages})
   end
 
   # Called when there's incoming data on the websocket for this channel
-  # def received(conversation)
-  #   ConversationChannel.broadcast_to(@conversation, {conversation: @conversation, users: @conversation.users, messages: @conversation.messages})
-  # end
   def received(data)
     byebug
     ActionCable.server.broadcast('conversation', 'ActionCable is connected')
