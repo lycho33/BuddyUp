a8970ea9fc56536d8a4d9b16702d084824b79a5a
lycho33 <cho.lyds@gmail.com>
2022-04-15 13:08:51 -0400

    added index controller for WordBank

--- Buddy-Up-api/app/controllers/wordbanks_controller.rb
+++ Buddy-Up-api/app/controllers/wordbanks_controller.rb
@@ -1,2 +1,8 @@
 class WordbanksController < ApplicationController
+    skip_before_action :require_login, only: [:index]
+    
+    def index
+        wordbank = Wordbank.all
+        render json: wordbank
+    end
 end
--- Buddy-Up-api/config/routes.rb
+++ Buddy-Up-api/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :wordbanks
   resources :conversations do
     resources :messages
   end
+  
   resources :users do
-    resources :word_banks
+    resources :wordbanks
   end
+
   post '/login', to: "auth#login"
   get '/auto_login', to: 'auth#auto_login'
   mount ActionCable.server => '/cable'
