dc17e5b6ebdca028a99a0702dfda541820f94033
lycho33 <cho.lyds@gmail.com>
2022-04-27 14:56:17 -0400

    render synonyms

--- buddy-up-frontend/src/Components/WordBank/Sentences.js
+++ buddy-up-frontend/src/Components/WordBank/Sentences.js
@@ -5,7 +5,7 @@ function Sentences({word}) {
     const wordbank = useSelector(state => state.wordbank)
     const wordInfo = wordbank.filter(w => w.word === word)
     const sentences = wordInfo[0].sentence
-  console.log(wordInfo, sentences)
+  // console.log(wordInfo, sentences)
 
     // const renderSentences = sentences.map(s => <div>
     //   <h3>{s.t}</h3>
--- buddy-up-frontend/src/Components/WordBank/Synonyms.js
+++ buddy-up-frontend/src/Components/WordBank/Synonyms.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-function Synonyms() {
+function Synonyms({word}) {
+    const wordbank = useSelector(state => state.wordbank)
+    const wordInfo = wordbank.filter(w => w.word === word)
+    const synonyms = wordInfo[0].synonyms
+    console.log(wordInfo, synonyms.map(sy => <li>{sy}</li>))
+
+
   return (
-    <div>Synonyms</div>
+    <div>
+        <h1>Synonyms</h1>
+        {synonyms.map((sy, i) => <li key={i}>{sy}</li>)}
+    </div>
   )
 }
 
