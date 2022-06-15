59b04d1be6a9fd5e27a276b948da76c8a202c4ba
lycho33 <cho.lyds@gmail.com>
2022-06-14 23:35:15 -0400

    render the correct datatype definition

--- buddy-up-frontend/src/Components/WordBank/Word.js
+++ buddy-up-frontend/src/Components/WordBank/Word.js
@@ -7,10 +7,13 @@ function Word({id, word, clickDefChallenges}) {
   const words = useSelector(state => state.wordbank)
   let idx = words.findIndex(w => w.word === word) 
   let definition = words[idx].definition
-  console.log(definition)
-  console.log(typeof definition)
+  console.log(words)
 
   const renderDefinition = () => {
+      if(typeof definition === 'object' && definition !== null){
+        definition = definition[0].definition
+      }
+      console.log(definition)
       if(definition !== null){ 
         return <span>{`${definition}`}</span>
       } else {
