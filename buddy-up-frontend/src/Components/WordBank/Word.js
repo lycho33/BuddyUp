660692705a8815055f1cd4337833faac039bbcdc
lycho33 <cho.lyds@gmail.com>
2022-06-15 14:02:46 -0400

    styling

--- buddy-up-frontend/src/Components/WordBank/Word.js
+++ buddy-up-frontend/src/Components/WordBank/Word.js
@@ -7,13 +7,8 @@ function Word({id, word, clickDefChallenges}) {
   const words = useSelector(state => state.wordbank)
   let idx = words.findIndex(w => w.word === word) 
   let definition = words[idx].definition
-  // console.log(definition)
 
   const renderDefinition = () => {
-      // if(typeof definition === 'object' && definition !== null){
-      //   definition = definition[0].definition
-      // }
-      // console.log(definition)
       if(definition !== null && typeof definition === 'string'){ 
         return <span>{`${definition}`}</span>
       } else {
@@ -25,13 +20,11 @@ function Word({id, word, clickDefChallenges}) {
     <>
       <tr key={id}>
         <td className='word'>{word}</td>
-        {/* do a conditional here to replace GUESS with the definition*/}
         <td 
           className='wordInfo'
           value={word} 
           onClick={clickDefChallenges}>
             {renderDefinition()}
-            {/* <span>Guess?</span> */}
         </td>
       </tr>
     </>
