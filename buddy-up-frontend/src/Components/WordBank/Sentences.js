14721a90a1c04ff0184bf2f3c75233dd07cdcbb1
lycho33 <cho.lyds@gmail.com>
2022-06-16 23:41:15 -0400

    sentences challenge styling

--- buddy-up-frontend/src/Components/WordBank/Sentences.js
+++ buddy-up-frontend/src/Components/WordBank/Sentences.js
@@ -15,7 +15,7 @@ function Sentences({word, display, setDisplay, sentences}) {
       <div className='sentences-container' style={{display: `${display}`}}>
           <h3>Sample Sentences</h3>
           <div className='sentences-list'>
-            {sentences && sentences.map(s => <li style={{margin: '10px 0'}}>{s}</li> )}
+            {sentences && sentences.map(s => <li style={{margin: '5px 0'}}>{s}</li> )}
           </div>
           <br />
           <button className='button-56' onClick={clickNext}>Next</button>
